3e7dbe77f5c57709702910ff2432b70718bc9bb6
jyun1 <59876614+jyun1desu@users.noreply.github.com>
2026-05-26 18:43:06 +0800

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const seen = new Map();
+
+        for(let i = 0; i < nums.length; i++) {
+            const cur = nums[i];
+            const toFind = target - cur;
+
+            if(seen.has(toFind)) {
+                return [seen.get(toFind), i]
+            } else {
+                seen.set(cur, i)
+            }
+        }
+
+        return []
+    }
+}
