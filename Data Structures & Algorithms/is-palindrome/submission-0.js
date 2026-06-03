652dba6a7eff2bb745a458b4bdada2d71ed791dc
jyun1 <59876614+jyun1desu@users.noreply.github.com>
2026-06-03 21:22:40 +0800

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+        let left = 0;
+        let right = cleaned.length - 1;
+        while(left < right) {
+            if(cleaned[left] !== cleaned[right]) {
+                return false;
+            }
+            left += 1;
+            right -=1;
+        };
+    
+        return true;
+    }
+}
